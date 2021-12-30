896313d58145840c9a44cd0b35e198f2f3ed55bd
Poojan Smart <44301271+PoojanSmart@users.noreply.github.com>
2021-12-30 13:41:39 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,35 @@
+import numpy as np
+nan=np.nan # represents impossible actions
+T = np.array([ # shape=[s, a, s']
+ [[0.7, 0.3, 0.0], [1.0, 0.0, 0.0], [0.8, 0.2, 0.0]],
+ [[0.0, 1.0, 0.0], [nan, nan, nan], [0.0, 0.0, 1.0]],
+ [[nan, nan, nan], [0.8, 0.1, 0.1], [nan, nan, nan]],
+ ])
+R = np.array([ # shape=[s, a, s']
+ [[10., 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]],
+ [[10., 0.0, 0.0], [nan, nan, nan], [0.0, 0.0, -50.]],
+ [[nan, nan, nan], [40., 0.0, 0.0], [nan, nan, nan]],
+ ])
+possible_actions = [[0, 1, 2], [0, 2], [1]]
+
+
+Q = np.full((3, 3), -np.inf) # -inf for impossible actions
+for state, actions in enumerate(possible_actions):
+	Q[state, actions] = 0.0 # Initial value = 0.0, for all possible actions
+
+learning_rate = 0.01
+discount_rate = 0.95
+n_iterations = 2
+
+for iteration in range(n_iterations):
+	Q_prev = Q.copy()
+	for s in range(3):
+		for a in possible_actions[s]:
+			Q[s, a] = np.sum([T[s, a, sp] * (R[s, a, sp] + discount_rate * np.max(Q_prev[sp])) for sp in range(3)])
+			print([[T[s, a, sp] , R[s, a, sp] ,  np.max(Q_prev[sp]) ,Q_prev[sp]] for sp in range(3)])
+			print([T[s, a, sp] * (R[s, a, sp] + discount_rate * np.max(Q_prev[sp])) for sp in range(3)])
+			print("--")
+
+	#print(Q)
+	#print(Q_prev)
+	#print("----------")
---
+++ quiz_game.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+#T = (state,action)
+T = [[[0 for i in range(12)],[0 for i in range(12)]]]
+
+#R = reward for a (state,action)
+R = [[10,0],[50,],100,500,1000,5000,10000,50000,100000,500000]
+
